53d94bc6de3f8a2d7f911d36f5eeadfabbc61a20
Yerbolat <twinspazyl@gmail.com>
2021-09-26 22:47:54 +0600

    saved-api init

---
+++ saved-api/src/main/java/kz/ead4/spring/savedapi/SavedApiApplication.java
@@ -0,0 +1,13 @@
+package kz.ead4.spring.savedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavedApiApplication.class, args);
+    }
+
+}
---
+++ saved-api/src/test/java/kz/ead4/spring/savedapi/SavedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ead4.spring.savedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

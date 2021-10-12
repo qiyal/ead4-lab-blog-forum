08039b47c5106974f9189188c0231fdad076068e
Dulat Sarsenbek <sarsenbek.d@gmail.com>
2021-10-12 09:36:15 +0600

    load balance

--- post-api/src/main/java/com/dula/demo/PostApiApplication.java
+++ post-api/src/main/java/com/dula/demo/PostApiApplication.java
@@ -2,7 +2,11 @@ package com.dula.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +16,11 @@ public class PostApiApplication {
         SpringApplication.run(PostApiApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+        return new RestTemplate(requestFactory);
+    }
 }

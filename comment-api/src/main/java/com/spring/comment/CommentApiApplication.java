f3f1f41cfc4e4559155e38e444a2d5c27eca6394
yer6o <2012.2012erbolat@gmail.com>
2021-10-26 15:09:35 +0600

    added swagger2

--- comment-api/src/main/java/com/spring/comment/CommentApiApplication.java
+++ comment-api/src/main/java/com/spring/comment/CommentApiApplication.java
@@ -3,10 +3,11 @@ package com.spring.comment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
-
+@EnableSwagger2
 public class CommentApiApplication {
 
     public static void main(String[] args) {

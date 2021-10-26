a1c6e3c0679773eacb4afdd9341c347e73767619
Dulat Sarsenbek <sarsenbek.d@gmail.com>
2021-10-26 14:48:30 +0600

    added swagger2 to auth-api, post-api

--- demo-spring/src/main/java/com/dula/demo/UserApiApplication.java
+++ demo-spring/src/main/java/com/dula/demo/UserApiApplication.java
@@ -9,9 +9,11 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class UserApiApplication {
 
     public static void main(String[] args) {

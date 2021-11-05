e815f645d094274379f27ad3c9cbb6377d1e849c
Yerbolat <twinspazyl@gmail.com>
2021-11-05 22:43:07 +0600

    add hystrix-dashboard for saved-api

--- saved-api/src/main/java/kz/ead4/spring/savedapi/SavedApiApplication.java
+++ saved-api/src/main/java/kz/ead4/spring/savedapi/SavedApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableEurekaClient
 @EnableSwagger2
 @EnableHystrix
+@EnableHystrixDashboard
 public class SavedApiApplication {
 
     public static void main(String[] args) {

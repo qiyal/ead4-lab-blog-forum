2b8e64d591839fb111fea541528db90839696e24
Yerbolat <twinspazyl@gmail.com>
2021-11-14 23:36:16 +0600

    change spring boot version 2.3.7.RELEASE and spring.cloud version Hoxton.SR8

--- demo-spring/src/main/java/com/dula/demo/repository/UserRepository.java
+++ demo-spring/src/main/java/com/dula/demo/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String name);
+    User getById(Long id);
 }

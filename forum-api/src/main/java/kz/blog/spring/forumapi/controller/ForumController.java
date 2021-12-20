81d8a2e7fadf4867bb61d8b1d7465afe13407fb8
duch2K <sarsenbek.d@gmail.com>
2021-12-20 23:39:55 +0600

    replace yerbo_dev with master

--- forum-api/src/main/java/kz/blog/spring/forumapi/controller/ForumController.java
+++ forum-api/src/main/java/kz/blog/spring/forumapi/controller/ForumController.java
@@ -12,52 +12,54 @@ import org.springframework.web.bind.annotation.*;
 public class ForumController {
     @Autowired
     private ForumService forumService;
+    private final static String publicApi = "/public";
+    private final static String privateApi = "/private";
 
     @ApiOperation(value = "Get all forums")
-    @GetMapping("/all")
+    @GetMapping(publicApi+ "/all")
     public ResponseEntity<?> getAllForums() {
         return ResponseEntity.ok(forumService.getAllForum());
     }
 
     @ApiOperation(value = "Get by forum id")
-    @GetMapping()
+    @GetMapping(publicApi)
     public ResponseEntity<?> getForumById(@RequestParam("forumId") Long forumId) {
         return ResponseEntity.ok(forumService.getForumById(forumId));
     }
 
     @ApiOperation(value = "Search forums by title field")
-    @GetMapping("/search")
+    @GetMapping(publicApi + "/search")
     public ResponseEntity<?> searchForumByTitle(@RequestParam("title") String title) {
         System.out.println("search" + " " + title);
         return ResponseEntity.ok(forumService.searchForumByTitle(title));
     }
 
     @ApiOperation(value = "Create new forum")
-    @PostMapping("/create")
+    @PostMapping(privateApi + "/create")
     public ResponseEntity<?> createForum(@RequestBody Forum forum) {
         return ResponseEntity.ok(forumService.createForum(forum));
     }
 
     @ApiOperation(value = "Delete by forum id")
-    @DeleteMapping("/delete/{forumId}")
+    @DeleteMapping(publicApi+ "/delete/{forumId}")
     public void deleteForum(@PathVariable("forumId") Long forumId) {
         forumService.deleteForum(forumId);
     }
 
     @ApiOperation(value = "Set forum owner by username")
-    @PatchMapping("/change-owner")
+    @PatchMapping(privateApi + "/change-owner")
     public ResponseEntity<?> updateForumOwnerByUsername(@RequestParam("forumId") Long forumId, @RequestParam("username") String username) {
         return ResponseEntity.ok(forumService.updateForumOwnerByUsername(forumId, username));
     }
 
     @ApiOperation(value = "Add member in forum")
-    @PatchMapping("/add-member")
+    @PatchMapping(privateApi + "/add-member")
     public ResponseEntity<?> addMember(@RequestParam("forumId") Long forumId, @RequestParam("username") String username) {
         return ResponseEntity.ok(forumService.addMember(forumId, username));
     }
 
     @ApiOperation(value = "remove member in forum")
-    @PatchMapping("/remove-member")
+    @PatchMapping(privateApi+ "/remove-member")
     public ResponseEntity<?> removeMember(@RequestParam("forumId") Long forumId, @RequestParam("memberId") Long memberId) {
         return ResponseEntity.ok(forumService.removeMember(forumId, memberId));
     }
